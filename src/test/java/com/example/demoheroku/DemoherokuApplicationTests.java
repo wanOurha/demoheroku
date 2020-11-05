d0f9fd41d204f0ca507fa97c8b9acdb20f0bc05f
wanourha <wanchana180@gmail.com>
2020-11-05 11:57:41 +0700

    first commit

---
+++ src/test/java/com/example/demoheroku/DemoherokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoherokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
